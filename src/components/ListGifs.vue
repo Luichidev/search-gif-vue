62df6cc8a650a354ddb979222aca61cfa95071c4
Luis Alberto Arana <luichidev@gmail.com>
2022-09-29 21:25:00 +0200

    feat: Add components

---
+++ src/components/ListGifs.vue
@@ -0,0 +1,37 @@
+<script setup>
+import CardGif from '@/components/CardGif.vue';
+
+defineProps({
+  arrayGifs: {
+    type: Array,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <section>
+    <div class='card-list'>
+      <CardGif 
+        v-for="gif in arrayGifs" 
+        :key='gif.id'
+        :title='gif.title'
+        :url='gif.url'
+        />
+    </div>
+  </section>
+</template>
+
+<style lang='css' scope>
+  .card-list {
+    display: grid;
+    min-height: 100vh;
+    grid-template-columns: repeat(auto-fit, minmax(var(--gridList-column), 1fr));
+    grid-auto-rows: var(--gridList-rows);
+    grid-template-rows: masonry;
+    grid-auto-flow: row dense;
+    grid-gap: var(--gridList-gap, 6px);
+    gap: var(--gridList-gap, 6px);
+    align-items: center;
+  }
+</style>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,46 @@
+<script setup>
+
+</script>
+
+<template>
+  <form class='search'>
+    <input class='search-input' type='text' placeholder='Buscar gifs...' />
+    <button class='search-submit'>Buscar</button>
+  </form>
+</template>
+
+<style lang='css' scope>
+  .search {
+    border-radius: 8px;
+    position: relative;
+    overflow: hidden;
+    margin: 16px 0;
+  }
+
+  .search-input {
+    font-size: 1rem;
+    width: 100%;
+    height: 44px;
+    border: 0;
+    padding: 8px 16px;
+  }
+
+  .search-submit {
+    background: red;
+    border: 0;
+    font-size: 1rem;
+    font-weight: bold;
+    height: 44px;
+    padding: 10px;
+    position: absolute;
+    right: 0;
+    transition: opacity 0.3s ease;
+    cursor: pointer;
+    backface-visibility: hidden;
+    color: #fff;
+  }
+
+  .search-input:hover {
+    opacity: 0.7;
+  }
+</style>
